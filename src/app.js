dff6c527be46216de6853cacc8cae77ed6b3373f
Shingo Tomioka <s.tomioka.103@gmail.com>
2017-05-20 12:27:59 +0900

    Create store

--- src/app.js
+++ src/app.js
@@ -1,34 +1,27 @@
-import expect from 'expect'
+import expect from 'expect';
+import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 1
+      return state + 1;
     case 'DECREMENT':
-      return state - 1
+      return state - 1;
     default:
-      return state
-  }
+      return state;
+  };
+};
+
+const store = createStore(counter);
+
+const render = () => {
+  document.body.innerText = store.getState();
 }
 
-expect(
-  counter(0, {type: 'INCREMENT'})
-).toEqual(1)
+store.subscribe(render);
 
-expect(
-  counter(1, {type: 'INCREMENT'})
-).toEqual(2)
+render();
 
-expect(
-  counter(2, {type: 'DECREMENT'})
-).toEqual(1)
-
-expect(
-  counter(1, {type: 'SOMETHING_ELSE'})
-).toEqual(1)
-
-expect(
-  counter(undefined, {})
-).toEqual(0)
-
-console.log('test passed!')
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
