b6721f4121c246ffa69086aaf676dbef6b6ba3b2
Shingo Tomioka <s.tomioka.103@gmail.com>
2017-05-20 12:57:06 +0900

    Add buttons

--- src/app.js
+++ src/app.js
@@ -3,8 +3,16 @@ import { createStore } from 'redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Counter = ({value}) => (
-  <h1>{value}</h1>
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+  }) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
 );
 
 const counter = (state = 0, action) => {
@@ -22,15 +30,21 @@ const store = createStore(counter);
 
 const render = () => {
   ReactDOM.render(
-    <Counter value={store.getState()} />,
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })}
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })}
+    />,
     document.getElementById('root')
-  )
+  );
 };
 
 store.subscribe(render);
 
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
-});
